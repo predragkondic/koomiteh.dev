e22caf29067803038ef01c5f9d694898eb97f59f
Peko <predragfm@gmail.com>
2026-05-14 14:45:54 +0200

    correct colors in admin list

--- apps/web/src/pages/AdminPostsListPage.tsx
+++ apps/web/src/pages/AdminPostsListPage.tsx
@@ -34,7 +34,6 @@ import AddIcon from "@mui/icons-material/Add";
 import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import DeleteIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/EditOutlined";
-import FilterListIcon from "@mui/icons-material/FilterList";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import RestoreIcon from "@mui/icons-material/RestoreOutlined";
 import VisibilityIcon from "@mui/icons-material/VisibilityOutlined";
@@ -349,7 +348,11 @@ function DesktopView({
             <col style={{ width: 140 }} />
           </colgroup>
           <TableHead>
-            <TableRow>
+            <TableRow
+              sx={{
+                backgroundColor: "surface.elevated",
+              }}
+            >
               {(
                 [
                   ["question", t("list.columns.question")],
@@ -365,12 +368,24 @@ function DesktopView({
                     direction={sortKey === key ? sortDir : "asc"}
                     onClick={() => cycleSort(key)}
                     aria-label={t("list.a11y.sortBy", { column: label })}
+                    sx={{
+                      color: "text.secondary",
+                      "&:hover": {
+                        color: "text.primary",
+                      },
+                      "&.Mui-active:hover": {
+                        color: "primary.main",
+                      },
+                      "& .MuiTableSortLabel-icon": {
+                        color: "inherit !important",
+                      },
+                    }}
                   >
                     {label}
                   </TableSortLabel>
                 </TableCell>
               ))}
-              <TableCell align="right" sx={{ pr: 1.5 }}>
+              <TableCell align="right" sx={{ textAlign: "center" }}>
                 {t("list.columns.actions")}
               </TableCell>
             </TableRow>
@@ -434,13 +449,15 @@ function DesktopView({
           <Typography variant="body2" color="text.disabled">
             {t("list.pager.info", { shown: totalShown, total: totalAll })}
           </Typography>
-          <Pagination
-            size="small"
-            shape="rounded"
-            count={pageCount}
-            page={currentPage}
-            onChange={(_, p) => setPage(p)}
-          />
+          {pageCount > 1 && (
+            <Pagination
+              size="small"
+              shape="rounded"
+              count={pageCount}
+              page={currentPage}
+              onChange={(_, p) => setPage(p)}
+            />
+          )}
         </Stack>
       </Card>
     </Box>
@@ -821,13 +838,6 @@ function MobileView({
           </Typography>
         </Stack>
         <Box sx={{ flex: 1 }} />
-        <Button
-          variant="outlined"
-          size="small"
-          startIcon={<FilterListIcon sx={{ fontSize: 16 }} />}
-        >
-          {t("list.filter")}
-        </Button>
       </Stack>
 
       {isLoading && (
